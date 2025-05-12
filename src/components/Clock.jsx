865183b6ea57e2fb7ec45d94f8eedffd9e8d3c66
nahid <y.nahid@avanto-vr.com>
2025-05-12 15:19:53 +0600

    Lifecycle in class component

--- src/components/Clock.jsx
+++ src/components/Clock.jsx
@@ -1,14 +1,46 @@
 import React from "react";
 
 class Clock extends React.Component {
+  // constructor(props) {
+  //   super(props);
+  //   this.state = { date: new Date() };
+  // }
+
+  //shortcut without constructior
+  state = { date: new Date() };
+
+  componentDidMount() {
+    this.clockTimer = setInterval(() => this.tick(), 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.clockTimer);
+  }
+
+  tick() {
+    this.setState({
+      date: new Date(),
+    });
+  }
+
   render() {
     return (
       <h1 className="heading">
-        <span className="text">{new Date()
-        .toLocaleTimeString(this.props.locale)}</span>
+        <span className="text">
+          {this.state.date.toLocaleTimeString(this.props.locale)}
+        </span>
       </h1>
     );
   }
 }
+
+//another way using functional component
+// function Clock({ locale }) {
+//   return (
+//     <h1 className='heading'>
+//       <span className='text'> {new Date().toLocaleTimeString(locale)}</span>
+//     </h1>
+//   )
+// }
 
 export default Clock;
