61e761930fd6dfa62f848dc1b957b1f4d38d68e6
nahid <y.nahid@avanto-vr.com>
2025-05-13 21:00:55 +0600

    Form Handling - Controlled vs Uncontrolled Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import Clock from "./components/Clock";
 import ClockList from "./components/ClockList";
+import Form from "./components/Form";
+import FROM from "./components/Form";
 
 function App() {
-  const quantities = [1, 2, 3];
   return (
     <div>
-      <ClockList quantities={quantities} />
+      <Form />
     </div>
   );
 }
